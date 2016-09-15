f7679ab55ea66f222859e46fe7a165a087540749
nmontielh <nmontielh@gmail.com>
2016-09-15 13:57:49 -0500

    Se hacen ajustes para los microservicios

--- micro-service-spring-mvc/src/main/java/org/microservices/app/Application.java
+++ micro-service-spring-mvc/src/main/java/org/microservices/app/Application.java
@@ -1,7 +1,5 @@
 package org.microservices.app;
 
-import java.util.Arrays;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -15,13 +13,12 @@ public class Application {
 		System.out.println("Let's inspect the beans provided by Spring Boot:");
 
 		String[] beanNames = ctx.getBeanDefinitionNames();
-		
-		
+
 		System.out.println("Total bean " + beanNames.length);
-		/*Arrays.sort(beanNames);
-		for (String beanName : beanNames) {
-			System.out.println(beanName);
-		}*/
+		/*
+		 * Arrays.sort(beanNames); for (String beanName : beanNames) {
+		 * System.out.println(beanName); }
+		 */
 	}
 
 }
